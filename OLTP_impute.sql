0ccd284c829ad53b550cd46a10454d15f361cc9d
tim <89766111+timothyckl@users.noreply.github.com>
2022-07-10 21:57:53 +0800

    Listed others files to be inserted

--- OLTP_impute.sql
+++ OLTP_impute.sql
@@ -1,31 +1,39 @@
 USE BikeSalesMinions
 GO
 
+-- category.txt
 BULK INSERT production.categories
 FROM 'C:\deng_ca2_data\category.txt'
 WITH (fieldterminator=' ', rowterminator='\n')
 GO
 
+-- brand.txt
 BULK INSERT production.brands
 FROM 'C:\deng_ca2_data\brand.txt'
 WITH (fieldterminator=' ', rowterminator='\n')
 GO
 
+-- stores.txt
 BULK INSERT sales.stores
 FROM 'C:\deng_ca2_data\stores.txt'
 WITH (fieldterminator='\t', rowterminator='\n')
 GO
-/**
-INSERT INTO sales.stores VALUES ('ST1','Santa Cruz Bikes','(831) 476-4321','santacruz@bikes.shop','3700 Portola Drive','Santa Cruz','CA',95060)
-INSERT INTO sales.stores VALUES ('ST2','Baldwin Bikes','(516) 379-8888','baldwin@bikes.shop','4200 Chestnut Lane','Baldwin','NY',11432)
-INSERT INTO sales.stores VALUES ('ST3','Rowlett Bikes','(972) 530-5555','rowlett@bikes.shop','8000 Fairway Avenue','Rowlett','TX',75088)
-**/
+
+-- staff.txt
 BULK INSERT sales.staffs
 FROM 'C:\deng_ca2_data\staff.txt'
 WITH (fieldterminator='\t', rowterminator='\n')
 GO
 
+-- stocks.csv
 
+-- customers.csv
+
+-- orderitems.csv
+
+-- orders.csv
+
+-- products.json
 Declare @Products varchar(max)
 Select @Products =
 BulkColumn
