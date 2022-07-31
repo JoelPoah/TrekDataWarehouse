2121f977b768a217ac7fd733026482b0fe89c7fc
unknown <joel.21@ichat.sp.edu.sg>
2022-07-31 18:12:23 +0800

    sort of done query 1 but realise it isn't really very useful maybe just take worst?

--- query_eda.sql
+++ query_eda.sql
@@ -2,9 +2,33 @@
 Performing exploratory data analysis to find out more about the data in this data warehouse and generate interesting
 insights so that final query to present can be selected
 */
-
+use BikeSalesDWMinions
+select * from SalesFacts
 
 -- Query 1
+
+--Select Top 100 Best products based on revenue and average discount
+select top 100 p.product_name, sum(sf.order_quantity*sf.list_price) as revenue,
+AVG(sf.discount) as discount
+from BikeSalesDWMinions..SalesFacts as sf 
+INNER JOIN BikeSalesDWMinions..[Product] as p ON sf.product_key = p.product_key
+group by p.product_name
+order by revenue desc, discount desc 
+
+
+select top 100 p.product_name , sum(sf.order_quantity*sf.list_price) as revenue,
+AVG(sf.discount) as discount, a.WorstProduct as WorstProducts, a.worstdiscount
+from BikeSalesDWMinions..SalesFacts as sf , BikeSalesDWMinions..Product as p,
+(select top 100 p.product_name as WorstProduct, sum(sf.order_quantity*sf.list_price) as revenue,
+AVG(sf.discount) as worstdiscount
+from BikeSalesDWMinions..SalesFacts as sf 
+INNER JOIN BikeSalesDWMinions..[Product] as p ON sf.product_key = p.product_key
+group by p.product_name
+order by revenue asc
+) as a
+WHERE sf.product_key=p.product_key
+group by p.product_name , a.WorstProduct,a.worstdiscount
+order by revenue desc
 
 
 -- Query 2
