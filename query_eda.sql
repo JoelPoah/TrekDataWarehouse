7b61d0d9fae36111a630dfedf97f81fe38a75eef
unknown <joel.21@ichat.sp.edu.sg>
2022-08-02 20:24:36 +0800

    juicy query 4

--- query_eda.sql
+++ query_eda.sql
@@ -54,26 +54,26 @@ where t.[rank] <= 3
 
 
 
--- Query 4
+-- Query 4 Sales/Orders/Customers
 /**Efficiency of company; find mean/median time taken in 
 delivering the bicycle(shipped_date - required_date) to various customers 
 (group by city in which they are in) if they are always late in delivery means
  got problem and should do something about the delivery in that particular city 
  **/
 use BikeSalesDWMinions
-select * from salesfacts
-
-select distinct c.city, MAX(DATEDIFF(Day,shipped.FullDateUK,received.FullDateUK)) as 'DayTaken' ,
-RANK () OVER (PARTITION BY)
-from SalesFacts as sf,time as shipped,
-time as received ,customer as c ,Store as st
-where sf.required_time_key = received.time_key and
+select c.state,c.city,AVG(order_quantity*list_price*(1-discount)) as revenue,
+AVG(DATEDIFF(Day,ordered.FullDateUK,shipped.FullDateUK)) as 'DayTaken' 
+,AVG(DATEDIFF(DAY,required.FullDateUK,shipped.FullDateUK)) as 'DayLate'
+from SalesFacts as sf,time as shipped,time as required,
+time as ordered,customer as c 
+where sf.order_time_key = ordered.time_key and required.time_key = sf.required_time_key and
 sf.ship_time_key = shipped.time_key and c.customer_key = sf.customer_key
-and sf.order_status = 4 and DATEDIFF(Day,shipped.FullDateUK,received.FullDateUK)>0
-GROUP BY c.city
-ORDER BY DayTaken DESC
-
-select * from time
-
-
+and sf.order_status = 4
+GROUP BY c.state,c.city
+ORDER BY DayTaken desc
+/**
+Conclusion cities in NY such as Tonawanda and Westbury are
+taking long time to deliver the bicycle.
+Revenue also justifies the delay in delivery.
+**/
 -- Query 5
