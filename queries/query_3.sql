9b1d7ab8878d7371aefa9144e70bd8dd7d321b4c
junnjiee <junnjiee16@gmail.com>
2022-08-03 16:42:49 +0800

    query 3 added

--- queries/query_3.sql
+++ queries/query_3.sql
@@ -13,3 +13,35 @@ select * from (
 	group by p.category_name, t.QuarterName, t.Quarter
 ) t
 where t.[rank] <= 3
+
+
+-- Sales of each bike category per quarter
+DROP VIEW IF EXISTS [pivoted_table];
+GO
+
+CREATE VIEW [pivoted_table] AS
+WITH pivot_data AS (
+	select 
+		t.Quarter,
+		p.category_name,
+		SUM(f.list_price * f.order_quantity * (1-f.discount)) 'revenue of category per quarter'
+	from SalesFacts f
+	inner join time t on f.ship_time_key = t.time_key
+	inner join product p on f.product_key = p.product_key
+	group by t.Quarter, p.category_name
+)
+SELECT [Quarter], [Mountain Bikes], [Road Bikes], [Cruisers Bicycles], [Electric Bikes], [Cyclocross Bicycles], [Comfort Bicycles], [Children Bicycles]
+FROM pivot_data
+PIVOT (max([revenue of category per quarter]) FOR [category_name] IN ([Mountain Bikes], [Road Bikes], [Cruisers Bicycles], [Electric Bikes], [Cyclocross Bicycles], [Comfort Bicycles], [Children Bicycles]))
+AS p
+GO
+
+select * from pivoted_table pt
+INNER JOIN (
+	SELECT t.[Quarter], SUM(f.list_price * f.order_quantity * (1-f.discount)) 'revenue per quarter'
+	FROM SalesFacts f
+	INNER JOIN [Time] t ON f.ship_time_key = t.time_key
+	GROUP BY t.[Quarter]
+) qt
+ON pt.[Quarter] = qt.[Quarter]
+GO
