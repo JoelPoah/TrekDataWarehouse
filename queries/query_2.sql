548e2b951312d9e2de6129e5e50ede7f755360ee
thermostat <tckldev@gmail.com>
2022-08-03 15:07:35 +0800

    Update Query 2

--- queries/query_2.sql
+++ queries/query_2.sql
@@ -1,8 +1,8 @@
 -- Query 2
 select 
-	s.staff_id, s.first_name, s.store_id,
-	round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2) 'revenue'
+	s.staff_id, concat(s.first_name, ' ', s.last_name) 'staff_name', s.store_id,
+	ISNULL(round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2), 0) 'revenue'
 from SalesFacts sf
-inner join staff s on sf.staff_key = s.staff_key
-group by s.staff_id, s.first_name, s.store_id
-order by [revenue] desc
+full outer join staff s on sf.staff_key = s.staff_key
+group by s.staff_id, concat(s.first_name, ' ', s.last_name), s.store_id
+order by s.store_id, [revenue] desc
