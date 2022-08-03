10a69f27b820766464d0f11c5d3d4b214af83480
tim <89766111+timothyckl@users.noreply.github.com>
2022-08-03 23:55:29 +0800

    Update query 2

--- queries/query_2.sql
+++ queries/query_2.sql
@@ -2,7 +2,7 @@
 use BikeSalesDWMinions
 
 select 
-	s.staff_id, concat(s.first_name, ' ', s.last_name) 'staff_name', s.store_id, 
+	s.store_id, s.staff_id, concat(s.first_name, ' ', s.last_name) as 'staff_name', 
 	case 
 		when s.staff_id in (
 			select distinct manager_id 
@@ -11,8 +11,12 @@ select
 		) then 'Manager'
 		else 'Normal'
 	end as 'staff_type',
-	isnull(round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2), 0) 'revenue'
-from SalesFacts sf
+	isnull(
+		round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2), 0
+	) as 'revenue',
+	rank() over(
+		partition by s.store_id 
+		order by isnull(round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2), 0) desc) as 'rank'
+from SalesFacts as sf
 full outer join staff s on sf.staff_key = s.staff_key
-group by s.staff_id, concat(s.first_name, ' ', s.last_name), s.store_id, s.manager_id
-order by s.store_id, [revenue] desc
+group by s.store_id, s.staff_id, concat(s.first_name, ' ', s.last_name)
