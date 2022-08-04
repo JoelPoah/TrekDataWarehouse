8a9fcc9430b8ade5c7f3827b8f51c3b063c05337
thermostat <tckldev@gmail.com>
2022-08-05 00:57:01 +0800

    Updated query 2

--- queries/query_2.sql
+++ queries/query_2.sql
@@ -14,10 +14,13 @@ select
 	end as 'staff_type',
 	isnull(
 		round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2), 0
-	) as 'revenue',
+	) / count(distinct year(t.FullDateUK)) as 'revenue',
 	rank() over(
 		partition by s.store_id 
 		order by isnull(round(sum(sf.order_quantity * sf.list_price * (1-sf.discount)), 2), 0) desc) as 'rank'
 from SalesFacts as sf
 full outer join staff s on sf.staff_key = s.staff_key
+inner join Time t on sf.ship_time_key = t.time_key
+where sf.order_status != 3
+and year(t.FullDateUK) in ('2016', '2017')
 group by s.store_id, s.staff_id, concat(s.first_name, ' ', s.last_name)
